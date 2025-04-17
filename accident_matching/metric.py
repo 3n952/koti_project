f240a3e46138d56b35d6a752c1a877c0fc6d0ef1
3n952 <smrlatks@ajou.ac.kr>
2025-04-17 17:36:27 +0900

    calculation metric module

--- accident_matching/metric.py
+++ accident_matching/metric.py
@@ -1,3 +1,6 @@
+import contextlib
+import io
+
 import pandas as pd
 import logging
 import geopandas as gpd
@@ -50,7 +53,7 @@ def evaluate(data: pd.DataFrame, processor_instance: AccidentMapMatchingProcesso
     '''
     recall@5, recall@1에 대한 평가지표를 산출
     '''
-    top5_answer_count = 0.0
+    # top5_answer_count = 0.0
     top1_answer_count = 0.0
 
     for idx, (_, row) in enumerate(data.iterrows()):
@@ -60,38 +63,39 @@ def evaluate(data: pd.DataFrame, processor_instance: AccidentMapMatchingProcesso
         taas_df.loc[:, 'occrrnc_time_dc'] = taas_df['occrrnc_time_dc'].str.replace('시', '', regex=True)  # timecode
         taas_df.loc[:, 'datetime'] = pd.to_datetime(taas_df['acdnt_dd_dc'] + ' ' + taas_df['occrrnc_time_dc'] + ':00:00')  # unixtime 변환
         taas_df.loc[:, 'unixtime'] = taas_df['datetime'].astype('int64') // 10**9 - (9 * 3600)  # KST 기준 맞추기
-        processor_instance.taas_sample_gdf = gpd.GeoDataFrame(
-            taas_df,
-            geometry=gpd.points_from_xy(row_df['x_crdnt_crdnt'], row_df['y_crdnt_crdnt']),
-            crs="EPSG:5179")
-        logging.info(f"{idx + 1} 번째 사고 데이터\n{processor_instance.taas_sample_gdf.head()}")
-        taas_sample_timestamp = processor_instance.taas_sample_gdf['unixtime'].iloc[0]
-        taas_sample_timecode = processor_instance.taas_sample_gdf['occrrnc_time_dc'].astype(int).iloc[0]
-        processor_instance.uptime = taas_sample_timestamp + 5400
-        processor_instance.downtime = taas_sample_timestamp - 1800
-        processor_instance.timecode = taas_sample_timecode
+        with contextlib.redirect_stdout(io.StringIO()), contextlib.redirect_stderr(io.StringIO()):
+            processor_instance.taas_sample_gdf = gpd.GeoDataFrame(
+                taas_df,
+                geometry=gpd.points_from_xy(row_df['x_crdnt_crdnt'], row_df['y_crdnt_crdnt']),
+                crs="EPSG:5179")
+            logging.info(f"{idx + 1} 번째 사고 데이터\n{processor_instance.taas_sample_gdf.head()}")
+            taas_sample_timestamp = processor_instance.taas_sample_gdf['unixtime'].iloc[0]
+            taas_sample_timecode = processor_instance.taas_sample_gdf['occrrnc_time_dc'].astype(int).iloc[0]
+            processor_instance.uptime = taas_sample_timestamp + 5400
+            processor_instance.downtime = taas_sample_timestamp - 1800
+            processor_instance.timecode = taas_sample_timecode
+            result = processor_instance.mapmatching_result(from_saved=False)
 
-        result = processor_instance.mapmatching_result(from_saved=False)
         result = result.reset_index()
-        top5_answer_count += recall_topN(n=5, ground_truth=answer_link_id, prediction_df=result)
+        # top5_answer_count += recall_topN(n=5, ground_truth=answer_link_id, prediction_df=result)
         top1_answer_count += recall_topN(n=1, ground_truth=answer_link_id, prediction_df=result)
 
-        metric_top5 = top5_answer_count / (idx + 1)
+        # metric_top5 = top5_answer_count / (idx + 1)
         metric_top1 = top1_answer_count / (idx + 1)
-        logging.info(f'[{idx + 1}번째 샘플까지]\nrecall@5: {metric_top5}\trecall@1: {metric_top1}')
-
-    metric_result_top5 = top5_answer_count / total_volume
+        # logging.info(f'[{idx + 1}번째 샘플까지]\nrecall@5: {metric_top5}\trecall@1: {metric_top1}')
+        logging.info(f'[{idx + 1}번째 샘플까지] recall@1: {metric_top1:.2f}')
+    # metric_result_top5 = top5_answer_count / total_volume
     metric_result_top1 = top1_answer_count / total_volume
 
-    return metric_result_top5, metric_result_top1
+    return metric_result_top1
 
 
 if __name__ == '__main__':
 
     logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
-    taas_data_path = 'taas_dataset/taas_under_100.csv'
-    ps_data_path = 'traj_sample/alltraj_20231211.txt'
+    taas_data_path = 'taas_dataset/test.csv'
+    ps_data_path = 'traj_sample/alltraj_20231213.txt'
     moct_network_path = 'moct_link/link'
     taas_sample = pd.read_csv(taas_data_path, encoding='cp949')
     total_volume = int(len(taas_sample))
@@ -99,7 +103,5 @@ if __name__ == '__main__':
 
     # 지표 계산
     logging.info(f'총 {total_volume}개 테스트 샘플에 대한 평가지표 산출중')
-    metric_result_top5, metric_result_top1 = evaluate(data=taas_sample,
-                                                      processor_instance=accidentlinkmatching,
-                                                      total_volume=total_volume)
-    logging.info(f"총 {total_volume}개의 테스트 샘플에 대한 평가지표 산출 완료.\nrecall@5: {metric_result_top5}\trecall@1: {metric_result_top1}")
+    metric_result_top1 = evaluate(data=taas_sample, processor_instance=accidentlinkmatching, total_volume=total_volume)
+    logging.info(f"총 {total_volume}개의 테스트 샘플에 대한 평가지표 산출 완료.\nrecall@1: {metric_result_top1}")
